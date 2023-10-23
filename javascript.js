671f34ea3e2f88601df365efc231de355cf73921
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2023-10-23 13:26:19 +1100

    Drawing of grid now dynamically fits within 1000px container boundary

--- javascript.js
+++ javascript.js
@@ -1,55 +1,55 @@
 addEventListener('DOMContentLoaded', () => {
 
-
-
 let container = document.querySelector('container');
+    container.setAttribute("class", "container");
 
 
 // set out initial default grid
 drawGrid(container, 16);
 
+// set drawing state and create 'p' to display draw state
+let clickState = 'un-clicked';
+statusP = document.createElement('p');
+
 // drawing logic
 
-drawColor();
-
+container.addEventListener('click', drawColor);
 
 // custom size grid button
 let gridSizeBtn = document.querySelector('.gridSize');
 
-gridSizeBtn.addEventListener('click', () => {
+    gridSizeBtn.addEventListener('click', () => {
     
-    let gridSize = 0;
-    do  {
-            gridSize = parseInt(prompt('How big your grid? No bigger than 100 boxes across please'));
-        }
+        let gridSize = 0;
+        do  {
+                gridSize = parseInt(prompt('How big your grid? No bigger than 100 boxes across please'));
+                if (!gridSize) {
+                    alert("You didn't enter anything! There will be no grid for you!");
+                    break;
+                    }                   
+            }      
+        while ((gridSize > 100) || (Number.isInteger(gridSize) == false));
         
-    while ((gridSize > 100) || (Number.isInteger(gridSize) == false));
-    console.log ('out of loop?')
+        // call drawGrid with custom grid size
+            drawGrid(container, gridSize);
 
-// call drawGrid with custom grid size
-drawGrid(container, gridSize);
-
-drawColor();
-
-
-
-})
-
+        }) // end gridSizeBtn Event Listener
 
 function drawGrid(container, gridSize) {
-    
     // clears out container
     container.innerHTML = "";
-    container.setAttribute("style",
-    "display: flex; flex-wrap: wrap; \
-    justify-content: center; align-items:center;")
 
-    for (let i = 1; i < (gridSize +1); i++) {
+      for (let i = 1; i < (gridSize +1); i++) {
 
         let rowContainer = document.createElement('div');
-        rowContainer.setAttribute("style",
-            "display:flex; flex-wrap: nowrap; flex-direction: row; \
-            justify-content: center; align-items: center;")
+
+        let rowHeight = Math.floor(1000 / gridSize);
+        rowContainer.style.height = `${rowHeight}px`;
+        console.log(rowHeight);
+
+
+        rowContainer.setAttribute("class", "rowContainer");
+            
         container.appendChild(rowContainer);
     
         for (let j = 1; j < (gridSize + 1); j++) {
@@ -57,47 +57,70 @@ function drawGrid(container, gridSize) {
             let squareDiv = document.createElement('div');
     
             // numbered squares, redundant
-            squareDiv.innerHTML = i;
+            //squareDiv.textContent = i;          
+            let boxSize = Math.ceil(1000 / gridSize);  //use math.ceil or floor I'm not sure
+             
+            squareDivSize = {
+                width: `${boxSize}px`,
+                height: `${boxSize}px`                 
+                };
+
+            squareDiv.setAttribute("class", "gridBox gridBoxBackgroundColor");
+           
+            squareDiv.style.width = squareDivSize.width;
+            squareDiv.style.height = squareDivSize.height;
             
- 
+            rowContainer.appendChild(squareDiv);   
+          
+
+
             
-            squareDiv.setAttribute("style", 
-                "border-style: solid;border-color: black; border-width: 1px; \
-                margin:0px; \
-                min-width: 50px; min-height:50px;\
-                flex: 0 0 50px")
-    
-            squareDiv.setAttribute("class", "gridBox")
-    
-            rowContainer.appendChild(squareDiv);        
+            
+
         }
-        console.log('next line)');
         let lineBreak = document.createElement('div');
-        lineBreak.setAttribute("style", "flex-basis: 100%; height: 0px;")
+        lineBreak.setAttribute("class", "lineBreak")
         container.appendChild(lineBreak);
-            
-    }
+        }
+    } // end function drawGrid
 
-}
+/*
+function toggleClickState() {
+
+    let statusDiv = document.querySelector('statusDiv');
+
+    if (clickState === 'un-clicked') {
+        statusP.textContent = "";
+        clickState ='clicked';      
+        statusP.textContent = clickState;
+        container.appendChild(statusP);     
+    } else {
+        statusP.textContent = "";
+        clickState = 'un-clicked';
+        statusP.textContent = clickState;
+        statusDiv.appendChild(statusP);       
+        }
+    } // end function toggleClickState
+8*/
 
 
 
-function drawColor() {
 
-    let gridBox = document.querySelectorAll(".gridBox");
+function drawColor(clickState) {
 
-
-    gridBox.forEach(function(b, ){
+    //toggleClickState()
+   
+    let gridBoxes = document.querySelectorAll(".gridBox");
     
-        b.addEventListener('mouseover', () => {
-            let colorValue = "blue";
+    gridBoxes.forEach(function(b){
+    
+        b.addEventListener('mouseover', function colorBox() {
     
             colorValueAsArray = ['0', '0', '0', '0', '0', '0'];
             
             for (let r = 0; r < 6; r++) {
                 
                 let colorValue = Math.floor(Math.random() * 16);
-                console.log(colorValue);
     
                 switch(colorValue) {
                     case 16:
@@ -127,23 +150,18 @@ function drawColor() {
                     }   
                 }
     
-            console.log(colorValueAsArray);
             colorValue = colorValueAsArray.join("");
-            console.log(colorValue);
-    
-            //let pink = "#ffe5ec";
-    
-            let gridColor = `#${colorValue}`;
-            b.setAttribute("style",        
-            `border-style: solid;border-color: black; border-width: 1px; \
-            margin:0px; \
-            min-width: 50px; min-height:50px;\
-            flex: 0 0 50px;\
-            background-color: ${gridColor}`)
-    
-        }) 
-    })
-    
-    }
 
-})
+            dynamicBoxStyle = {
+                backgroundColor: `#${colorValue}`                
+                };
+            b.style.backgroundColor = dynamicBoxStyle.backgroundColor;
+            })      
+        })
+    
+    } //end function drawColor
+
+
+
+
+}) //end program

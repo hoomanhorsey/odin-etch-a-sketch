ba2e05daa4f7aa721dcd78681c6c0e3770701386
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2023-10-27 01:05:32 +1100

    Cleanup before merge

--- javascript.js
+++ javascript.js
@@ -3,36 +3,10 @@ addEventListener('DOMContentLoaded', () => {
 let container = document.querySelector('container');
     container.setAttribute("class", "container");
 
+// Default grid size
 let gridSize = 16;
 
-let isActiveB = true;
-let isActiveE = true;
-let isActiveC = true;
-
-let isColoringEnabled = false;
-
-// Determining whether mouse is over container
-let isMouseOverContainer = false;
-
-container.addEventListener('mouseenter', () =>{
-    isMouseOverContainer = true;
-    }
-        )
-container.addEventListener('mouseleave', () =>{
-    isMouseOverContainer = false;
-    }
-        )
-
-document.body.addEventListener('click', () => {
-    if (isMouseOverContainer === true) {
-        if (isColoringEnabled === false) {
-            isColoringEnabled = true;
-        } else {
-            isColoringEnabled = false;
-
-        }
-     }})
-
+// Listeners to determine mouse button up or down, for drawing
 let mouseStatus = 'up';
 
 container.addEventListener('mousedown', () => {
@@ -46,27 +20,22 @@ container.addEventListener('mousedown', () => {
      });
 
 // choose menu item
-
 selectedMenuBtn = document.querySelectorAll('.menuBtn');
 
 let blackBtn = document.querySelector('#blackBtn');
 let randomBtn = document.querySelector('#randomBtn');
 let eraseBtn = document.querySelector('#eraseBtn');
 
-
 selectedMenuBtn.forEach((e) =>
     e.addEventListener('click', (e)=> {
         switch(e.target.textContent){
-            case 'black':
-                
+            case 'black':               
                 e.target.style.backgroundColor = "lightblue";
                 randomBtn.style.backgroundColor = "bisque";
                 eraseBtn.style.backgroundColor = "bisque";
-
-                isColoringEnabled = false;
-
                 draw("black");
                 break;
+
             case 'random':
                 e.target.style.backgroundColor = "lightblue";
                 blackBtn.style.backgroundColor = "bisque";
@@ -82,48 +51,33 @@ selectedMenuBtn.forEach((e) =>
                 draw("white");
                 break;
         
-            case 'refresh':
+            case 'clear':
                 randomBtn.style.backgroundColor = "bisque";
                 blackBtn.style.backgroundColor = "bisque";
                 eraseBtn.style.backgroundColor = "bisque";
+                drawGrid(container, gridSize);
+                break;
+
+            case 'resize':
+                randomBtn.style.backgroundColor = "bisque";
+                blackBtn.style.backgroundColor = "bisque";
+                eraseBtn.style.backgroundColor = "bisque";
+                
+                do  {
+                    gridSize = parseInt(prompt('How big your grid? No bigger than 100 boxes across please'));
+                    if (!gridSize) {
+                        alert("You didn't enter anything! There will be no grid for you!");
+                        break;
+                        }                   
+                }      
+                while ((gridSize > 100) || (Number.isInteger(gridSize) == false));
 
                 drawGrid(container, gridSize);
-                //drawRefresh();      
-                break;
-        }  
-    }
-    )
-)
-
+              } } ) )
 
 // set out initial default grid
-drawGrid(container, 16);
+drawGrid(container, gridSize    );
 
-// set drawing state and create 'p' to display draw state
-let clickState = 'un-clicked';
-statusP = document.createElement('p');
-
-
-
-// custom size grid button
-let gridSizeBtn = document.querySelector('.gridSize');
-
-    gridSizeBtn.addEventListener('click', () => {
-    
-        //let gridSize = 0;
-        do  {
-                gridSize = parseInt(prompt('How big your grid? No bigger than 100 boxes across please'));
-                if (!gridSize) {
-                    alert("You didn't enter anything! There will be no grid for you!");
-                    break;
-                    }                   
-            }      
-        while ((gridSize > 100) || (Number.isInteger(gridSize) == false));
-        
-        // call drawGrid with custom grid size
-            drawGrid(container, gridSize);
-
-        }) // end gridSizeBtn Event Listener
 
 function drawGrid(container, gridSize) {
     // clears out container
@@ -165,20 +119,19 @@ function drawGrid(container, gridSize) {
     } // end function drawGrid
 
 
-
 function draw(color, colorType) {
     
     let gridBoxes = document.querySelectorAll(".gridBox");
  
     gridBoxes.forEach(function(b){     
     
-        b.addEventListener('mouseover', () => {
+        b.addEventListener('mousemove', () => {
             if (colorType === 'random') {
                 color = generateRanColor();
             }
             console.log("c", color);        
 
-            if (/*(isColoringEnabled === true) && (*/mouseStatus === "down") 
+            if (mouseStatus === "down") 
                 {
                     b.style.backgroundColor = color;
                 }      
@@ -186,17 +139,10 @@ function draw(color, colorType) {
         }
     )}
 
-
-
-
-
-
 function generateRanColor() {
-
 colorValueAsArray = ['0', '0', '0', '0', '0', '0'];
                 
-                for (let r = 0; r < 6; r++) {
-                    
+                for (let r = 0; r < 6; r++) {                   
                     let colorValue = Math.floor(Math.random() * 16);
         
                     switch(colorValue) {
@@ -224,24 +170,17 @@ colorValueAsArray = ['0', '0', '0', '0', '0', '0'];
                         default:
                             colorValueAsArray[r] = colorValue; 
                             break;               
-                        }   
-                    }
-                
+                        }}
                 return "#"+colorValueAsArray.join("");
-
-
-
-
-} //end function genRanColor
-
-
-
+        } //end function genRanColor
 
 
 }) //end program
 
 
-
+// set drawing state and create 'p' to display draw state
+// let clickState = 'un-clicked';
+// statusP = document.createElement('p');
 /*
 function toggleClickState() {
 
@@ -260,3 +199,19 @@ function toggleClickState() {
         }
     } // end function toggleClickState
 */
+
+// Listening to determining clicks on drawing grid to turn pen on and off
+// let isColoringEnabled = false;
+// let isMouseOverContainer = false;
+
+// container.addEventListener('mouseenter', () =>{
+//     isMouseOverContainer = true;})
+// container.addEventListener('mouseleave', () =>{
+//     isMouseOverContainer = false;})
+
+// document.body.addEventListener('click', () => {
+//     if (isMouseOverContainer === true) {
+//         if (isColoringEnabled === false) {
+//             isColoringEnabled = true;
+//         } else {isColoringEnabled = false;
+//         }}})

336878c98fd87d6e98adaac8f3677e459df13d04
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2023-10-26 12:58:59 +1100

    Added new functionality: black pen + eraser, mouse down to draw, refresh grid, menu buttons

--- javascript.js
+++ javascript.js
@@ -3,6 +3,99 @@ addEventListener('DOMContentLoaded', () => {
 let container = document.querySelector('container');
     container.setAttribute("class", "container");
 
+let gridSize = 16;
+
+let isActiveB = true;
+let isActiveE = true;
+let isActiveC = true;
+
+let isColoringEnabled = false;
+
+// Determining whether mouse is over container
+let isMouseOverContainer = false;
+
+container.addEventListener('mouseenter', () =>{
+    isMouseOverContainer = true;
+    }
+        )
+container.addEventListener('mouseleave', () =>{
+    isMouseOverContainer = false;
+    }
+        )
+
+document.body.addEventListener('click', () => {
+    if (isMouseOverContainer === true) {
+        if (isColoringEnabled === false) {
+            isColoringEnabled = true;
+        } else {
+            isColoringEnabled = false;
+
+        }
+     }})
+
+let mouseStatus = 'up';
+
+container.addEventListener('mousedown', () => {
+     mouseStatus = 'down';
+     console.log(mouseStatus)
+     });
+
+ container.addEventListener('mouseup', () => { 
+     mouseStatus = 'up';
+     console.log(mouseStatus)
+     });
+
+// choose menu item
+
+selectedMenuBtn = document.querySelectorAll('.menuBtn');
+
+let blackBtn = document.querySelector('#blackBtn');
+let randomBtn = document.querySelector('#randomBtn');
+let eraseBtn = document.querySelector('#eraseBtn');
+
+
+selectedMenuBtn.forEach((e) =>
+    e.addEventListener('click', (e)=> {
+        switch(e.target.textContent){
+            case 'black':
+                
+                e.target.style.backgroundColor = "lightblue";
+                randomBtn.style.backgroundColor = "bisque";
+                eraseBtn.style.backgroundColor = "bisque";
+
+                isColoringEnabled = false;
+
+                draw("black");
+                break;
+            case 'random':
+                e.target.style.backgroundColor = "lightblue";
+                blackBtn.style.backgroundColor = "bisque";
+                eraseBtn.style.backgroundColor = "bisque";
+                let colorType = "random";
+                draw("random", colorType);    
+                break;
+                
+            case 'eraser':
+                e.target.style.backgroundColor = "lightblue";
+                randomBtn.style.backgroundColor = "bisque";
+                blackBtn.style.backgroundColor = "bisque";
+                draw("white");
+                break;
+        
+            case 'refresh':
+                randomBtn.style.backgroundColor = "bisque";
+                blackBtn.style.backgroundColor = "bisque";
+                eraseBtn.style.backgroundColor = "bisque";
+
+                drawGrid(container, gridSize);
+                //drawRefresh();      
+                break;
+        }  
+    }
+    )
+)
+
+
 // set out initial default grid
 drawGrid(container, 16);
 
@@ -10,16 +103,14 @@ drawGrid(container, 16);
 let clickState = 'un-clicked';
 statusP = document.createElement('p');
 
-// drawing logic
 
-container.addEventListener('click', drawColor);
 
 // custom size grid button
 let gridSizeBtn = document.querySelector('.gridSize');
 
     gridSizeBtn.addEventListener('click', () => {
     
-        let gridSize = 0;
+        //let gridSize = 0;
         do  {
                 gridSize = parseInt(prompt('How big your grid? No bigger than 100 boxes across please'));
                 if (!gridSize) {
@@ -44,7 +135,6 @@ function drawGrid(container, gridSize) {
 
         let rowHeight = Math.floor(1000 / gridSize);
         rowContainer.style.height = `${rowHeight}px`;
-        console.log(rowHeight);
 
         rowContainer.setAttribute("class", "rowContainer");
             
@@ -53,9 +143,7 @@ function drawGrid(container, gridSize) {
         for (let j = 1; j < (gridSize + 1); j++) {
     
             let squareDiv = document.createElement('div');
-    
-            // numbered squares, redundant
-            //squareDiv.textContent = i;          
+           
             let boxSize = Math.ceil(1000 / gridSize);  //use math.ceil or floor I'm not sure
              
             squareDivSize = {
@@ -76,60 +164,76 @@ function drawGrid(container, gridSize) {
         }
     } // end function drawGrid
 
-function drawColor(clickState) {
 
-    //toggleClickState()
-   
+
+function draw(color, colorType) {
+    
     let gridBoxes = document.querySelectorAll(".gridBox");
+ 
+    gridBoxes.forEach(function(b){     
     
-    gridBoxes.forEach(function(b){
-    
-        b.addEventListener('mouseover', function colorBox() {
-    
-            colorValueAsArray = ['0', '0', '0', '0', '0', '0'];
-            
-            for (let r = 0; r < 6; r++) {
-                
-                let colorValue = Math.floor(Math.random() * 16);
-    
-                switch(colorValue) {
-                    case 16:
-                        colorValueAsArray[r] = 'G'; 
-                        break;
-                    case 15: 
-                        colorValueAsArray[r] = 'F'; 
-                        break;
-                    case 14:
-                        colorValueAsArray[r] = 'E'; 
-                        break;
-                    case 13:
-                        colorValueAsArray[r] = 'D'; 
-                        break;
-                    case 12:
-                        colorValueAsArray[r] = 'C'; 
-                        break;
-                    case 11:
-                        colorValueAsArray[r] = 'B'; 
-                        break;
-                    case 10:
-                        colorValueAsArray[r] = 'A'; 
-                        break;
-                    default:
-                        colorValueAsArray[r] = colorValue; 
-                        break;               
-                    }   
-                }
-    
-            colorValue = colorValueAsArray.join("");
+        b.addEventListener('mouseover', () => {
+            if (colorType === 'random') {
+                color = generateRanColor();
+            }
+            console.log("c", color);        
 
-            dynamicBoxStyle = {
-                backgroundColor: `#${colorValue}`                
-                };
-            b.style.backgroundColor = dynamicBoxStyle.backgroundColor;
-            })      
-        })
-    
-    } //end function drawColor
+            if (/*(isColoringEnabled === true) && (*/mouseStatus === "down") 
+                {
+                    b.style.backgroundColor = color;
+                }      
+            })
+        }
+    )}
+
+
+
+
+
+
+function generateRanColor() {
+
+colorValueAsArray = ['0', '0', '0', '0', '0', '0'];
+                
+                for (let r = 0; r < 6; r++) {
+                    
+                    let colorValue = Math.floor(Math.random() * 16);
+        
+                    switch(colorValue) {
+                        case 16:
+                            colorValueAsArray[r] = 'G'; 
+                            break;
+                        case 15: 
+                            colorValueAsArray[r] = 'F'; 
+                            break;
+                        case 14:
+                            colorValueAsArray[r] = 'E'; 
+                            break;
+                        case 13:
+                            colorValueAsArray[r] = 'D'; 
+                            break;
+                        case 12:
+                            colorValueAsArray[r] = 'C'; 
+                            break;
+                        case 11:
+                            colorValueAsArray[r] = 'B'; 
+                            break;
+                        case 10:
+                            colorValueAsArray[r] = 'A'; 
+                            break;
+                        default:
+                            colorValueAsArray[r] = colorValue; 
+                            break;               
+                        }   
+                    }
+                
+                return "#"+colorValueAsArray.join("");
+
+
+
+
+} //end function genRanColor
+
 
 
 

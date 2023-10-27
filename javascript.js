81e78db5fec09702f031a223fd21eff9bca8fc6f
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2023-10-27 16:25:22 +1100

    Added file save and load functionality - very rough, needs clean up

--- javascript.js
+++ javascript.js
@@ -11,12 +11,10 @@ let mouseStatus = 'up';
 
 container.addEventListener('mousedown', () => {
      mouseStatus = 'down';
-     console.log(mouseStatus)
      });
 
  container.addEventListener('mouseup', () => { 
      mouseStatus = 'up';
-     console.log(mouseStatus)
      });
 
 // choose menu item
@@ -73,10 +71,23 @@ selectedMenuBtn.forEach((e) =>
                 while ((gridSize > 100) || (Number.isInteger(gridSize) == false));
 
                 drawGrid(container, gridSize);
+                break;
+
+            case 'save image':
+                saveImage();
+                break;
+            case 'load image':
+                loadImage();
+                break
+
+            case 'save to file':
+                let file = saveImage();
+                console.log(file);
+                generateTextFileUrl(file);
               } } ) )
 
 // set out initial default grid
-drawGrid(container, gridSize    );
+drawGrid(container, gridSize);
 
 
 function drawGrid(container, gridSize) {
@@ -86,8 +97,8 @@ function drawGrid(container, gridSize) {
       for (let i = 1; i < (gridSize +1); i++) {
 
         let rowContainer = document.createElement('div');
-
         let rowHeight = Math.floor(1000 / gridSize);
+
         rowContainer.style.height = `${rowHeight}px`;
 
         rowContainer.setAttribute("class", "rowContainer");
@@ -106,6 +117,7 @@ function drawGrid(container, gridSize) {
                 };
 
             squareDiv.setAttribute("class", "gridBox gridBoxBackgroundColor");
+            squareDiv.setAttribute('id', `${i}, ${j}`)
            
             squareDiv.style.width = squareDivSize.width;
             squareDiv.style.height = squareDivSize.height;
@@ -129,7 +141,10 @@ function draw(color, colorType) {
             if (colorType === 'random') {
                 color = generateRanColor();
             }
-            console.log("c", color);        
+            // Silence this console.log, however note that the function fires all colors all the time
+            // This is a bug, but it doesn't affect functionality at the moment. 
+            // But I keep the console.log, deactivated, as a note to self
+            // console.log("c", color);        
 
             if (mouseStatus === "down") 
                 {
@@ -175,6 +190,132 @@ colorValueAsArray = ['0', '0', '0', '0', '0', '0'];
         } //end function genRanColor
 
 
+function saveImage() {
+
+    // reinsert functionality once you have figured out the save file.
+    //let fileName = prompt("Please enter a file name")
+    //fileName = fileName + ".etch"
+    //alert(fileName);
+
+    const saveFile = {fileType:'etch', imageSize:gridSize, imageData:[]}
+    //console.log(saveFile)
+
+    container = document.querySelector('.container');
+    gridBoxes = document.querySelectorAll('.gridBox');
+    gridBoxes.forEach( (e) => {
+        saveFile['imageData'].push(e.style.backgroundColor);
+        }
+            );
+
+    
+    for (let i = 1; i < (gridSize +1); i++) {
+        //rowContainer = document.querySelector('.rowContainer');
+        //console.log(rowContainer);
+
+        for (let j = 1; j < (gridSize + 1); j++) {
+            //gridBox = document.querySelector('.gridBox');
+            //console.log(gridBox);   
+
+        }
+
+    }
+
+    const saveFileJSON = JSON.stringify(saveFile);
+    return saveFileJSON;
+
+    const objectAgain = JSON.parse(saveFileJSON);
+
+    } //end function saveImage
+
+function loadImage(saveFile) {
+    //saveFile = {fileType:'etch', imageSize:2, imageData:['rgb(114, 33, 173)', '', '', 'rgb(216, 46, 252)']}
+    
+    // check file
+
+    if (saveFile['fileType'] != 'etch') {
+        alert('Sorry. Incorrect file type');
+    }
+
+
+    drawGrid(container, saveFile.imageSize);
+
+    gridBoxes = document.querySelectorAll('.gridBox');
+    let colorRef = 0;
+    gridBoxes.forEach( (e) => {
+           {
+                                    
+                e.style.backgroundColor = saveFile.imageData[colorRef];
+                colorRef++;
+        }
+       }   );
+    }
+
+
+// A global variable should be defined to hold the URL for the file to be downloaded
+// This is good practice as if many links are being generated or the link is being 
+// regularly updated, you don't want to be creating new variables every time, wasting memory
+var textFileUrl = null;
+
+// Function for generating a text file URL containing given text
+function generateTextFileUrl(txt) {
+    let fileData = new Blob([txt], {type: 'text/plain'});
+    // If a file has been previously generated, revoke the existing URL
+    if (textFileUrl !== null) {
+        window.URL.revokeObjectURL(textFileUrl);
+    }
+    textFileUrl = window.URL.createObjectURL(fileData);
+    // Returns a reference to the global variable holding the URL
+    // Again, this is better than generating and returning the URL 
+    // itself from the function as it will eat memory if the file contents are large or regularly changing
+    document.getElementById('downloadLink').href = textFileUrl;
+    
+    //document.getElementById('downloadLink').href = textFileUrl;
+
+    //return textFileUrl;
+    };
+    // Generate the file download URL and assign it to the link
+    // Wait until the page has loaded! Otherwise the download link element will not exist
+    // window.addEventListener("load", function(){
+        
+    //     document.getElementById('downloadLink').href = generateTextFileUrl("boo hoo");
+    // });
+
+
+
+
+
+
+// Get references to the input element and the pre element for displaying file contents.
+const fileInput = document.getElementById('fileInput');
+const fileContents = document.getElementById('fileContents');
+
+// Add an event listener to the file input element.
+fileInput.addEventListener('change', function() {
+    const selectedFile = fileInput.files[0];
+
+    if (selectedFile) {
+        const reader = new FileReader();
+
+        reader.onload = function(event) {
+            const contents = event.target.result;
+            const objectAgain = JSON.parse(contents);
+            loadImage(objectAgain);
+            //fileContents.textContent = contents;
+        };
+
+        reader.readAsText(selectedFile);
+        console.log(selectedFile);
+    } else {
+        fileContents.textContent = 'No file selected.';
+    }})
+
+
+
+
+
+
+
+
 }) //end program
 
 
@@ -214,4 +355,4 @@ function toggleClickState() {
 //         if (isColoringEnabled === false) {
 //             isColoringEnabled = true;
 //         } else {isColoringEnabled = false;
-//         }}})
+        // }}})
